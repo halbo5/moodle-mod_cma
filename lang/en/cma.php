fd65e438328a68f0b88ac1554bf4dfe56bfe4844
Alain Bolli <alain.bolli@gmail.com>
2017-08-16 18:11:46 +0200

    English strings added

--- lang/en/cma.php
+++ lang/en/cma.php
@@ -32,106 +32,69 @@ $string['cma:view'] = 'View CMA content';
 $string['pluginadministration'] = 'Page module administration';
 $string['pluginname'] = 'CMA';
 $string['search:activity'] = 'CMA';
-$string['timeopen'] = 'Date d\'ouverture du test';
-$string['timeclose'] = 'Date de fermeture du test';
-$string['word1'] = 'différencier';
-$string['word2'] = 'essayer';
-$string['word3'] = 's\'impliquer';
-$string['word4'] = 'être pratique';
-$string['word5'] = 'réceptif';
-$string['word6'] = 'logique';
-$string['word7'] = 'méthodique';
+$string['timeopen'] = 'Test open time';
+$string['timeclose'] = 'Test closing time';
+$string['word1'] = 'discriminating';
+$string['word2'] = 'tentative';
+$string['word3'] = 'involved';
+$string['word4'] = 'practical';
+$string['word5'] = 'receptive';
+$string['word6'] = 'relevant';
+$string['word7'] = 'analytical';
 $string['word8'] = 'impartial';
-$string['word9'] = 'ressentir';
-$string['word10'] = 'faire attention';
-$string['word11'] = 'réfléchir';
-$string['word12'] = 'faire';
-$string['word13'] = 'accepter';
-$string['word14'] = 'prendre des risques';
-$string['word15'] = 'évaluer';
-$string['word16'] = 'prendre conscience';
-$string['word17'] = 'intuitif';
-$string['word18'] = 'productif';
-$string['word19'] = 'logique';
-$string['word20'] = 'interrogateur';
-$string['word21'] = 'abstrait';
-$string['word22'] = 'observateur';
-$string['word23'] = 'concret';
-$string['word24'] = 'actif';
-$string['word25'] = 'orienté vers le présent';
-$string['word26'] = 'réfléchissant';
-$string['word27'] = 'orienté vers le futur';
-$string['word28'] = 'pragmatique';
-$string['word29'] = 'partir de son expérience';
-$string['word30'] = 'observer';
-$string['word31'] = 'penser';
-$string['word32'] = 'expérimenter';
+$string['word9'] = 'feeling';
+$string['word10'] = 'watching';
+$string['word11'] = 'thinking';
+$string['word12'] = 'doing';
+$string['word13'] = 'accepting';
+$string['word14'] = 'risk-taker';
+$string['word15'] = 'évaluating';
+$string['word16'] = 'aware';
+$string['word17'] = 'intuitive';
+$string['word18'] = 'productive';
+$string['word19'] = 'logical';
+$string['word20'] = 'questionning';
+$string['word21'] = 'abstract';
+$string['word22'] = 'observing';
+$string['word23'] = 'concrete';
+$string['word24'] = 'active';
+$string['word25'] = 'present oriented';
+$string['word26'] = 'reflecting';
+$string['word27'] = 'futur oriented';
+$string['word28'] = 'practical';
+$string['word29'] = 'experimentation';
+$string['word30'] = 'observation';
+$string['word31'] = 'conceptualization';
+$string['word32'] = 'experience';
 $string['word33'] = 'intense';
-$string['word34'] = 'réservé';
-$string['word35'] = 'rationnel';
-$string['word36'] = 'responsable';
-$string['convergent'] = 'Convergent';
-$string['assimilateur'] = 'Assimilateur';
-$string['divergent'] = 'Divergent';
-$string['accomodateur'] = 'Accomodateur';
-$string['modeapprentissage'] = 'Modes d\'apprentissages';
+$string['word34'] = 'reserve';
+$string['word35'] = 'rational';
+$string['word36'] = 'responsible';
+$string['convergent'] = 'Converging';
+$string['assimilateur'] = 'Assimilating';
+$string['divergent'] = 'Diverging';
+$string['accomodateur'] = 'Accommodating';
+$string['modeapprentissage'] = 'Learning styles';
 $string['points'] = "Points";
-$string['decouvrezmodes'] = "Découvrez vos modes d'apprentissages dominants";
-$string['experienceconcrete'] = "Expérience concrète";
-$string['observationreflechie'] = "Observation réfléchie";
-$string['conceptualisationabstraite'] = "Conceptualisation abstraite";
-$string['experimentationactive'] = "Expérimentation active";
-$string['profilapprentissage'] = "Profil d'apprentissage";
+$string['decouvrezmodes'] = "Discover your principals learning styles";
+$string['experienceconcrete'] = "Concrete Experience";
+$string['observationreflechie'] = "Reflective Observation";
+$string['conceptualisationabstraite'] = "Abstract Conceptualization";
+$string['experimentationactive'] = "Active Experimentation";
+$string['profilapprentissage'] = "Learning profile";
 $string['profildesc'] = "Le profil indique dans quelle mesure vous privilégiez une approche abstraite plutôt que concrète et l'action plutôt que la réflexion";
-$string['yourprofile'] = "Votre profil";
-$string['accomodateurdesc'] = "Un accommodateur a des orientations opposées à celles de l'assimilateur. Il réussit mieux dans les domaines de l'expérience concrète et de l'expérimentation active. Il est
-surtout capable de réaliser des choses — en mettant en pratique des plans et des expériences - et
-de s'engager dans de nouvelles expériences. Il a tendance à prendre davantage de risques que les
-sujets caractéristiques des trois autres catégories. On a retenu le terme accommodateur car un tel
-sujet réussit particulièrement bien dans des situations ou il s'agit de s'adapter à des circonstances
-spéciﬁques. Dans des situations dans lesquelles une théorie ne correspond pas aux faits, cette
-personne remet le plan ou la théorie en question. Elle a tendance à résoudre les problèmes d'une
-manière intuitive par essai et erreur, s'appuyant plus sur l'information des autres que sur leur
-aptitude à l'analyse. Elle est à l'aise avec les autres mais est souvent perçue comme impatiente et
-brusque. Sa formation se situe souvent dans le domaine technique ou pratique comme les
-affaires. On retrouve des personnes ayant ce mode d'apprentissage dans des métiers orientés vers
-l'action, comme les métiers du commerce.";
-$string['divergentdesc'] = "Le divergent a des possibilités d'apprentissage opposées à celles du convergent. Un tel sujet est le plus à l'aise dans une situation d'expérience concrète et d'observation
-réﬂéchie. Sa force réside dans l'imagination. Il parvient très bien à percevoir des situations
-concrètes en adoptant des perspectives variées. Nous avons appelé ce style divergent car une
-personne ayant ce mode d'apprentissage réussit mieux dans des situations qui supposent la
-création d'idées comme par exemple des sessions de brainstorming. La recherche montre que les
-divergents s'intéressent à autrui et font preuve d'imagination et d'émotion. Ils ont des intérêts
-culturels étendus et tendent à se spécialiser dans le domaine artistique. Ce mode est
-caractéristique de sujets ayant fait des études littéraires et artistiques. Les conseillers, les
-spécialistes du développement, les chefs du personnel sont souvent caractérisés par ce mode d'apprentissage.";
-$string['assimilateurdesc'] = "Un assimilateur a des aptitudes d'apprentissage dans le domaine de la conceptualisation abstraite et de l'observation réﬂéchie. Une telle personne a surtout des possibilités
-dans le domaine de la création des modèles théoriques. Elle réussit particulièrement bien dans le
-domaine du raisonnement inductif et dans l'organisation d'observations disparates en une
-explication cohérente. Elle est, comme le convergent, moins intéressée par les autres que par les
-concepts abstraits ; mais elle se sent moins concernée par l'emploi pratique des théories. Pour un
-tel sujet, il est plus important que la théorie soit satisfaisante d'un point de vue logique ; dans une
-situation dans laquelle une théorie ne s'accommoderait pas aux faits, l'assimilateur aurait plutôt
-tendance à négliger ou a réexaminer les faits. Ce mode d'apprentissage est plutôt caractéristique
-des sciences de base et des mathématiques que des sciences appliquées. On trouve ce mode
-d'apprentissage chez les praticiens de la recherche ou de la planiﬁcation.";
-$string['convergentdesc'] = "Les aptitudes d'apprentissage dominantes du convergent sont la conceptualisation
-abstraite et l'expérimentation active. La force la plus importante de ce type de sujet
-réside dans l'application pratique des idées. Une personne ayant ce style semble le mieux réussir
-dans des situations analogues aux tests d'intelligence, où il n'y a qu'une réponse correcte ou
-solution à une question ou un problème. La connaissance de cette personne est organisée de telle
-sorte qu'elle peut se centrer sur des problèmes spéciﬁques en utilisant une pensée hypothético-déductive.
-La recherche concernant ce mode d'apprentissage montre que les convergents sont
-relativement peu émotifs, préférant s'occuper de choses plutôt que de personnes. Ils ont des
-intérêts techniques étroits et cherchent à se spécialiser en sciences physiques. Ce mode
-d'apprentissage est caractéristique de bon nombre d'ingénieurs.";
+$string['yourprofile'] = "Your profile";
+$string['accomodateurdesc'] = "The Accommodating learning style is 'hands-on', and relies on intuition rather than logic. These people use other people's analysis, and prefer to take a practical, experiential approach. They are attracted to new challenges and experiences, and to carrying out plans. They commonly act on 'gut' instinct rather than logical analysis. People with an Accommodating learning style will tend to rely on others for information than carry out their own analysis. This learning style is prevalent and useful in roles requiring action and initiative. People with an Accommodating learning style prefer to work in teams to complete tasks. They set targets and actively work in the field trying different ways to achieve an objective.";
+$string['divergentdesc'] = "These people are able to look at things from different perspectives. They are sensitive. They prefer to watch rather than do, tending to gather information and use imagination to solve problems. They are best at viewing concrete situations several different viewpoints. Kolb called this style 'Diverging' because these people perform better in situations that require ideas-generation, for example, brainstorming. People with a Diverging learning style have broad cultural interests and like to gather information. They are interested in people, tend to be imaginative and emotional, and tend to be strong in the arts. People with the Diverging style prefer to work in groups, to listen with an open mind and to receive personal feedback.";
+$string['assimilateurdesc'] = "The Assimilating learning preference is for a concise, logical approach. Ideas and concepts are more important than people. These people require good clear explanation rather than practical opportunity. They excel at understanding wide-ranging information and organising it a clear logical format. People with an Assimilating learning style are less focused on people and more interested in ideas and abstract concepts. People with this style are more attracted to logically sound theories than approaches based on practical value. These learning style people is important for effectiveness in information and science careers. In formal learning situations, people with this style prefer readings, lectures, exploring analytical models, and having time to think things through.";
+$string['convergentdesc'] = "People with a Converging learning style can solve problems and will use their learning to find solutions to practical issues. They prefer technical tasks, and are less concerned with people and interpersonal aspects. People with a Converging learning style are best at finding practical uses for ideas and theories. They can solve problems and make decisions by finding solutions to questions and problems. People with a Converging learning style are more attracted to technical tasks and problems than social or interpersonal issues. A Converging learning style enables specialist and technology abilities. People with a Converging style like to experiment with new ideas, to simulate, and to work with practical applications.";
 $string['me'] = 'Me';
 $string['all'] = 'All courses';
 $string['coursetitle'] = "This course";
-$string['ec'] = 'ec';
-$string['or'] = 'or';
-$string['ca'] = 'ca';
-$string['ea'] = 'ea';
+$string['ec'] = 'ce';
+$string['or'] = 'ro';
+$string['ca'] = 'ac';
+$string['ea'] = 'ae';
 $string['allprofiles'] = "All profiles";
 $string['comparerscore'] = 'Compare your scores with other users';
 $string['titleallaverage'] = 'Your scores compared with those from all courses';
@@ -150,8 +113,8 @@ $string['aboutdesc'] = "<p>Cette application se base sur le test CMA (Caractéri
 <p>Contact : alain.bolli@gmail.com</p>";
 $string['maniere'] = "Ma manière d'apprendre";
 $string['consigne'] = "Vous allez parcourir 9 écrans avec à chaque fois 4 mots. Vous devez classer les mots de chaque écran en affectant d'un 4 le mot qui caractérise le mieux votre mode d'apprentissage, puis un 3 pour le mot suivant, jusqu'à 1 pour le mot qui vous caractérise le moins bien <strong>lorsque vous êtes en train d'apprendre</strong>.";
-$string['serie'] = "Série de mots";
-$string['cliquez'] = "Cliquez sur un mot pour affecter";
+$string['serie'] = "Word serie";
+$string['cliquez'] = "Click on a word to give";
 $string['point'] = "point";
 $string['plural'] = "s";
 $string['continuer'] = "Continue";
